6eb3323c08dc9d00346844b8e452f09453639dc8
Nihar <nihar1kashyap7@gmail.com>
2022-08-08 12:13:14 +0530

    Tables created

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from sys import prefix
 from fastapi import FastAPI
 from routes.v1.v1_router import v1_router
-from db.connect import SessionLocal, engine
 
 from core.config import settings
 from db.session import engine   #new
-from db.base_class import Base  #new
+from db.base import Base  #new
 
 
 app = FastAPI()
 
+print("Create tables")
 Base.metadata.create_all(bind=engine)
 
 app.include_router(v1_router, prefix='/api/v1')
